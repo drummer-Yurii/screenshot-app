c3fd46b4d25c196f19ae1288a0d1a7896a9864b8
Yurii Rybachok <drummer2009@ukr.net>
2023-11-05 09:57:56 +0200

    create frontend & setup

---
+++ client/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>Take a screenshot</h1>
+    </header>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
