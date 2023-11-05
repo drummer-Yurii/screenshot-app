f4f684caed792e424e17c7553a85c6ba597f668d
Yurii Rybachok <drummer2009@ukr.net>
2023-11-05 10:28:12 +0200

    logic for take screenshot from url & display him

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,21 +4,44 @@
       <h1>Take a screenshot</h1>
     </header>
     <main>
-      <form @submit.prevent="">
-        <input type="url" name="url" id="url" />
+      <form @submit.prevent="requestScreenshot">
+        <input type="url" name="url" id="url" v-model="url" />
         <input type="submit" value="Take screenshot" />
       </form>
-      <a href="#">
-        <img src="" />
+      <a v-if="screenshotUrl != ''" :href="screenshotUrl" target="_blank" download>
+        <img :src="screenshotUrl" />
       </a>
     </main>
   </div>
 </template>
 
 <script>
-
+import { ref } from 'vue';
 export default {
   name: 'App',
+  setup() {
+    const url = ref('http://');
+    const screenshotUrl = ref('');
+
+    const requestScreenshot = async () => {
+      const res = await fetch('http://localhost:5000/screenshot', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          url: url.value
+        })
+      }).then(data => data.json());
+      screenshotUrl.value = `http://localhost:5000/static/screenshots/${res.ID}.png`;
+    }
+
+    return {
+      url,
+      screenshotUrl,
+      requestScreenshot,
+    }
+  }
 }
 </script>
 
@@ -30,5 +53,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+img {
+  display: block;
+  width: 400px;
+  max-width: 100%;
+  margin: 32px auto;
 }
 </style>
--- server/server.js
+++ server/server.js
@@ -10,35 +10,34 @@ app.use(cors());
 app.use(express.json());
 
 app.post('/screenshot', async (req, res) => {
-    // Set path & ID
-    const ID = (new Date().getTime().toString(36));
-    const loc = "./public/screenshots/" + ID + ".png";
+  // Set path & ID
+  const ID = new Date().getTime().toString(36);
+  const loc = './public/screenshots/' + ID + '.png';
 
+  // Start browser
+  const browser = await puppeteer.launch({
+    defaultViewport: {
+      width: 1920,
+      height: 1080,
+    },
+    ignoreDefaultArgs: ['--disable-extensions'],
+  });
 
-    // Start browser
-    const browser = await puppeteer.launch({
-        defaultViewport: {
-            width: 1920,
-            height: 1080,
-        },
-        ignoreDefaultArgs: ['--disable-extensions'],
-    });
+  // Open page
+  const page = await browser.newPage();
+  await page.goto(req.body.url);
 
-    // Open page
-    const page = await browser.newPage();
-    page.goto(req.body.url);
+  // Take a screenshot
+  await page.screenshot({ path: loc });
 
-    // Take a screenshot
-    await page.screenshot({ path: loc });
+  await browser.close();
 
-    await browser.close();
-
-    res.json({
-        success: true,
-        ID
-    })
+  res.json({
+    success: true,
+    ID,
+  });
 });
 
 app.listen(5000, () => {
-    console.log("Server has started on port 5000");
+  console.log('Server has started on port 5000');
 });
